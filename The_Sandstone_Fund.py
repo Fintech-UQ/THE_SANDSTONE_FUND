fe27a5b2396dfc31860596e34fc798697fb726ea
Oliver Stack <Oliver.stack@uqconnect.edu.au>
2021-07-13 23:04:24 +1000

    Final Version: SIG x UNSW Algo2021

--- The_Sandstone_Fund.py
+++ The_Sandstone_Fund.py
@@ -1,102 +1,217 @@
-
-import numpy as np
 import math
+import os
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+data_history = 66
+np.seterr("ignore")
 
 
-def get_prices_until_the_last_monday(prcSoFar):
-    '''
-    determine the number of days since monday. this is because our position
-    only changes every monday so we need to calculate what our position
-    is if it where to be calculated from the last moday
-    '''
-    tmp = len(prcSoFar[0])%5 - 1
-    number_of_days_since_moday = 4 if tmp == -1 else tmp
+class Stock:
 
-    # gets the index of the most recent monday
-    current_index = len(prcSoFar[0]) - 1
-    monday_index = current_index - number_of_days_since_moday
+    def __init__(self, stock_number, historic_price):
+        self.price = historic_price[-1]
+        self.number = stock_number
+        self.historic_prices = historic_price
+        self.training_days = []
+        self.vol_training = []
+        self.linear_model = LinearRegression()
+        self.current_day = len(historic_price)
+        self.scaled_stocks = []
+        self.vibe = 0
+        self.z = 0
+        self.is_vol = False
+        self.vol = 0
 
-    # gets an array that includes the prices up until the most recent moday
-    prices_up_until_monday = prcSoFar.T[:monday_index].T
-    return prices_up_until_monday
+    def set_vibe_training_days(self, days):
+        """
+        Sets the vibe training days
+        """
+        self.training_days = self.historic_prices[-days:]
+
+    def set_historic_days(self, hd):
+        """
+        Sets the amount of days we have access too
+        """
+        self.historic_prices = hd
+
+    def is_volatile(self, days):
+        """
+        Checks if the stock is volatile
+        :param days: Amount of days to check over
+        :return: None
+        """
+        self.vol_training = self.historic_prices[-days:]
+        total_negative_change = 0
+        total_positive_change = 0
+        for i in range(1, days):
+            change = self.vol_training[i] - self.vol_training[i - 1]
+            if change > 0:
+                total_positive_change += change
+            else:
+                total_negative_change += change
+        if (total_positive_change - total_negative_change) / self.price > 0.15:
+            self.is_vol = True
+        self.vol = (total_positive_change - total_negative_change) / self.price
+
+    def update_model(self, days):
+        """
+        Update the linear regression model being used
+        :param days: Linear regression model time period from today's day
+        :return: None
+        """
+        self.linear_model = LinearRegression()
+        x = np.array(list(range(self.current_day - days, self.current_day))).reshape(-1, 1)
+        self.linear_model.fit(x, self.scaled_stocks[-days:])
+
+    def get_r_squared(self, days):
+        """
+        Return the r2 value of the current stocks linear model
+        :param days: The amount of time to check the r2 value on the LR
+        :return: None
+        """
+        x = np.array(list(range(self.current_day - days, self.current_day))).reshape(-1, 1)
+        r2 = self.linear_model.score(x, self.scaled_stocks[-days:])
+        return r2
+
+    def scale_stocks(self):
+        """
+        Scales all stocks down for easier computations
+        """
+        stock = []
+        start_price = self.training_days[0]
+        for price in self.training_days[1:]:
+            stock.append(price / start_price)
+        self.scaled_stocks = stock
+
+    def update_vibe(self, periods, coefficients, r2_cutoffs):
+        """
+        Calculates the 'vibe' of the stock at the current point in time
+        :param periods: Time periods
+        :param coefficients: Weighting of each time period
+        :param r2_cutoffs: r2 cutoff necessary for each period to even be considered for a position
+        :return: none
+        """
+        total_vibe = 0
+        for (period, coefficient, r2_cutoff) in zip(periods, coefficients, r2_cutoffs):
+            self.update_model(period)
+            if self.get_r_squared(period) < r2_cutoff:
+                self.vibe = 0
+                return
+            else:
+                total_vibe += 10 * coefficient * self.linear_model.coef_ * self.get_r_squared(period)
+        self.vibe = (total_vibe / sum(periods)) * 1000
+
+    def get_today_investment(self):
+        """
+        :return: returns the amount to invest in one stock based on the stocks variables
+        """
+        if self.is_vol:
+            if self.z > 1:
+                return -10000
+            elif self.z < -1:
+                return 10000
+            else:
+                return 0
+        else:
+            if self.vibe > 5:
+                return 5000
+            elif self.vibe > 4:
+                return 7000
+            elif self.vibe > 3:
+                return 7500
+            elif self.vibe > 2:
+                return 9000
+            elif self.vibe > 1:
+                return 9500
+            else:
+                return 0
+
+    def set_z_value(self):
+        """
+        Sets Z value of stock
+        """
+        self.z = (self.price - np.mean(self.vol_training)) / np.std(self.vol_training)
 
 
-def get_the_market(prHst):
-    # get the intial amount of shares owned by puting one dolar in each of them 
-    # the total value should be 250
-    # then for each day plot the value 
-    number_of_shares_owned_for_each_stock = []
-    for stock in prHst:
-        #add the number of shares owned on day 1 (index 0) if $1 is invested
-        number_of_shares = 1/stock[0]
-        number_of_shares_owned_for_each_stock.append(number_of_shares)
-    # this is value not number of stocks
+def write_csv(position):
+    """
+    Function creates a csv file if one doesn't exist and writes today's positions to be used for tomorrow
+    :param position: Positions to store for tomorrow
+    :return: None
+    """
+    data_frame = pd.DataFrame(position).T
+    if os.path.isfile("./previous_days.csv"):
+        df = pd.read_csv("./previous_days.csv")
+        df.loc[len(df)] = position
+        os.remove("./previous_days.csv")
+        df.to_csv("./previous_days.csv", index=False)
+    else:
+        data_frame.to_csv("./previous_days.csv", index=False)
 
-    return number_of_shares_owned_for_each_stock
 
-
-def getMyPosition (prcSoFar):
-    return get_the_market(prcSoFar)
-    # if 13 weeks have not passed then we do not take a position
-    if len(prcSoFar) <= 65:
-        return np.zeros(100)
-    # up until and including the last monday
-    prices_up_until_monday = get_prices_until_the_last_monday(prcSoFar)
-
-    # rank all of the stocks make a list of tuples (index, return)
-    '''
-    our momentum strategy uses past 12-week return minus 
-    most recent month’s return to rank stocks
-    '''
-    # gets a list of all the stocks and their returns 
-    stock_returns = []
-    for i in range(len(prices_up_until_monday)):
-        stock = prices_up_until_monday[i]
-        index = len(stock) - 1
-        # the monday 13 weeks ago
-        start_monday_index = index - 65
-        # the friday 12 weeks after 
-        end_friday_index = index - 6
-
-        # percent return 
-        percent_return = ((stock[end_friday_index] - stock[start_monday_index])/stock[end_friday_index])*100
-        period = stock[start_monday_index:end_friday_index]
-        vol = np.std(period)*len(period)
-       
-        momentum_return = percent_return
-        stock_returns.append((i, momentum_return, vol))
-    
-    # sorts them in order of their return 
-    stock_returns.sort(key = lambda x: x[1], reverse=True) 
-    print('THE VERY BEGINNING')
-    print('THE VERY BEGINNING')
-    print('THE VERY BEGINNING')
-    print('THE VERY BEGINNING')
-    print(stock_returns)
-
-    '''
-    make a list of zeros then loop through the top 33 ranked stocks
-    and set each value to be the number of shares which will be 10,000/price
-    '''
-    
+def getMyPosition(prcSoFar):
+    """
+    Function goes through each stock and evaluates if a position should be held or is currently being held. After which
+    if it is determined that a position would be advantageous, the array at the stocks index is updated to reflect.
+    :param prcSoFar: Prices of the index at a given day where day == len(prcSoFar)
+    :return: len(prcSoFar) dimensional array where each [index] represents a position in 'index' stock
+    """
     position = np.zeros(100)
 
-    for i in range(10):
-        stock_index = stock_returns[i][0]
-        percent_return = stock_returns[i][1]
-        price = prices_up_until_monday[stock_index][-1]
-        number_of_shares = math.floor(5000/price)
-        percent_return = stock_returns[i][1]
-        if percent_return > 0:
-            position[stock_index] = number_of_shares
-    
-    
+    # Clean previous memory
+    if prcSoFar.shape[1] == 1 and os.path.isfile("./previous_days.csv"):
+        os.remove("./previous_days.csv")
+        write_csv(position)
+    elif prcSoFar.shape[1] == 1 and not os.path.isfile("./previous_days.csv"):
+        write_csv(position)
+
+    first_buy_day = (prcSoFar.shape[1] - data_history) - 1
+
+    # If the amount of data current avialable does not meat our constant value return no position in any stock
+    if prcSoFar.shape[1] <= data_history:
+        return position
+    yesterday_positions = pd.read_csv("./previous_days.csv")
+
+    # Data Setting Up
+    stocks = []
+    for i in range(100):
+        vibe_periods = [15, 30, 65]
+        vibe_coefficients = [4, 1, 4]
+        vibe_r2_cutoffs = [0.5, 0.5, 0.8]
+
+        # Create a new stock object and initialize values
+        new_stock = Stock(i, prcSoFar[i])
+        new_stock.is_volatile(10)
+
+        # For stocks that are NOT volatile we have set a 3 day holding period minimum
+        if not new_stock.is_vol:
+            if first_buy_day % 3 != 0:
+                new_price = prcSoFar[:, :(prcSoFar.shape[1] - (first_buy_day % 3))]
+                new_stock.set_historic_days(new_price[i])
+                new_stock.is_volatile(10)
+
+        new_stock.set_vibe_training_days(66)
+        new_stock.scale_stocks()
+        new_stock.set_z_value()
+        new_stock.update_vibe(vibe_periods, vibe_coefficients, vibe_r2_cutoffs)
+        stocks.append(new_stock)
+
+    # Method
+    for index in range(100):
+        investment = stocks[index].get_today_investment()
+        position[index] = math.floor(investment / prcSoFar[index][-1])
+
+        # Checks if we currently have a position and if so will not update the position as to reduce
+        # constantly updating position sizes (non volatile stock check only)
+        if yesterday_positions.iloc[-1][f"{index}"] != 0 and not stocks[index].is_vol and position[index] > 0:
+            if (yesterday_positions.iloc[-1][f"{index}"] * prcSoFar[index][-1]) > 10000 or \
+                    (yesterday_positions.iloc[-1][f"{index}"] * prcSoFar[index][-1]) < -10000:
+                position[index] = math.floor(9500 / prcSoFar[index][-1])
+            else:
+                position[index] = yesterday_positions.iloc[-1][f"{index}"]
+
+    # Store today's position as for tomorrow
+    write_csv(position)
     return position
-
-
-
-
-
-
-
-
--- eval.py
+++ eval.py
@@ -1,14 +1,11 @@
 #!/usr/bin/envblah python
-
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from teamName import getMyPosition as getPosition
-from plots_and_tools import get_the_market
+from The_Sandstone_Fund import getMyPosition as getPosition
 
 nInst = 0
 nt = 0
-commRate = 0.0000
+commRate = 0.0050
 dlrPosLimit = 10000
 
 def loadPrices(fn):
@@ -18,26 +15,8 @@ def loadPrices(fn):
     return df.values.T
 
 
-# pricesFile = "./prices250.txt"
-# prcAll = loadPrices(pricesFile)
-
-etherum_prices = pd.read_csv("./prices250.csv")
-nt, nInst = etherum_prices.values.shape
-prcAll = etherum_prices.values.T
-
-
-def plot_price(prices):
-    x = list(range(0, 250))
-    index = prcAll[97]
-    # index = get_the_market(prcAll)
-    fig, ax = plt.subplots()
-    ax.plot(x, prices, color="red", marker="o", markersize=2.5)
-    ax.set_ylabel("Earnings $", fontsize=14)
-    ax.set_xlabel("Trading Days", fontsize=14)
-    ax2 = ax.twinx()
-    ax2.plot(x, index, color="blue", marker="o", markersize=2.5)
-    ax2.set_ylabel("Index Price $", fontsize=14)
-    plt.show()
+pricesFile = "./prices250.txt"
+prcAll = loadPrices(pricesFile)
 
 
 def print_results(mean_pl, returns, sharpe_value, d_vol):
@@ -47,7 +26,7 @@ def print_results(mean_pl, returns, sharpe_value, d_vol):
     print("annSharpe(PL): %.2lf " % sharpe_value)
     print("totDvolume: %.0lf " % d_vol)
 
-values_over_data = []
+
 def calcPL(prcHist):
     cash = 0
     curPos = np.zeros(nInst)
@@ -80,7 +59,6 @@ def calcPL(prcHist):
         todayPL = cash + posValue - value
         todayPLL.append(todayPL)
         value = cash + posValue
-        values_over_data.append(value)
         ret = 0.0
         if (totDVolume > 0):
             ret = value / totDVolume
@@ -97,4 +75,3 @@ def calcPL(prcHist):
 
 (meanpl, ret, sharpe, dvol, value) = calcPL(prcAll)
 print_results(meanpl, ret, sharpe, dvol)
-plot_price(values_over_data)
